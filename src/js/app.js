0023b2693defe6e39eaed1e45ff96b0833253dee
CoraRosmery <corarosmery@gmail.com>
2024-06-04 23:04:23 +0200

    final

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   let slideIndex = 1;
   showSlides(slideIndex);
 
-  // Next/previous controls
+ 
   function plusSlides(n) {
-    console.log(n);
     showSlides(slideIndex += n);
   }
 
-  // Thumbnail image controls
+
   function currentSlide(n) {
     showSlides(slideIndex = n);
   }
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     dots[slideIndex-1].className += " active";
   }
 
-  // Event listeners for controls
+
   document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
   document.querySelector('.next').addEventListener('click', () => plusSlides(1));
   document.querySelectorAll('.dot').forEach((dot, index) => {
@@ -38,32 +37,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   console.log(n)
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
