38325182c7dbc4a8434210e4846a1d3afd9863a2
CoraRosmery <corarosmery@gmail.com>
2024-06-22 18:40:31 +0200

    imagenes cambio

---
+++ dist/presentacion.8f0c9192.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) slideIndex = 1;
+        if (n < 1) slideIndex = slides.length;
+        for(i = 0; i < slides.length; i++)slides[i].style.display = "none";
+        for(i = 0; i < dots.length; i++)dots[i].className = dots[i].className.replace(" active", "");
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+    document.querySelector(".prev").addEventListener("click", ()=>plusSlides(-1));
+    document.querySelector(".next").addEventListener("click", ()=>plusSlides(1));
+    document.querySelectorAll(".dot").forEach((dot, index)=>{
+        dot.addEventListener("click", ()=>currentSlide(index + 1));
+    });
+});
+
+//# sourceMappingURL=presentacion.8f0c9192.js.map
