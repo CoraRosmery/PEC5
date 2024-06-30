bf0a44d40d6e486147108e45b48cc66b0965ff02
CoraRosmery <corarosmery@gmail.com>
2024-06-30 21:24:18 +0200

    Change imgs resolution

---
+++ dist/bibliografia.c94d5cdf.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".lazyLink").forEach(t=>{t.addEventListener("click",function(){let t=this.getAttribute("data-href");window.location.href=t})})});
+//# sourceMappingURL=bibliografia.c94d5cdf.js.map
---
+++ dist/discos.33f57926.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".lazyLink").forEach(t=>{t.addEventListener("click",function(){let t=this.getAttribute("data-href");window.location.href=t})})});
+//# sourceMappingURL=discos.33f57926.js.map
