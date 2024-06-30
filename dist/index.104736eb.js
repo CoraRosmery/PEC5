32b22726728284aaa703b542bad478dddcd3322a
CoraRosmery <corarosmery@gmail.com>
2024-06-30 13:58:18 +0200

    Change img prin

---
+++ dist/index.104736eb.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("lazyLink").addEventListener("click",function(){let t=this.getAttribute("data-href");window.location.href=t})});
+//# sourceMappingURL=index.104736eb.js.map
